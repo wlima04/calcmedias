8503392087354f6274b92905e35cb08785576e33
Welliton <wellitonchagas2018@gmail.com>
2023-07-14 14:53:15 -0300

    Adicionando função de calcular media

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const form = document.getElementById('form')
+const txtAtiv = document.getElementById('txtAtiv')
+const txtNota = document.getElementById('txtNota')
+const spanRes = document.getElementById('res')
 const imgAprovado = '<img src="./images/aprovado.png" alt="Imagem Aprovado"></img>'
 const imgReprovado = '<img src="./images/reprovado.png" alt="Imagem Reprovado"></img>'
 const atividades = []
@@ -11,22 +14,23 @@ form.addEventListener('submit', function(e){
 
     addLinha()
     atualizaTabela()
-    mediaFinal()
+    atualizaMediaFinal()
 })
 
 function addLinha() {
-    const txtAtiv = document.getElementById('txtAtiv')
-    const txtNota = document.getElementById('txtNota')
-
+    if (atividades.includes(txtAtiv.value)) {
+        alert('[ERRO] A atividade já foi inserida, tente novamente!')
+    } else {
+        let linha = '<tr>'
+        linha += `<td>${txtAtiv.value}</td>`
+        linha += `<td>${txtNota.value}</td>`
+        linha += `<td>${txtNota.value >= 6 ? imgAprovado : imgReprovado}</td>`
+        linha += `</tr>`
+        linhas += linha
+    }
+    
     atividades.push(txtAtiv.value)
     notas.push(Number(txtNota.value))
-
-    let linha = '<tr>'
-    linha += `<td>${txtAtiv.value}</td>`
-    linha += `<td>${txtNota.value}</td>`
-    linha += `<td>${txtNota.value >= 6 ? imgAprovado : imgReprovado}</td>`
-    linha += `</tr>`
-    linhas += linha
 
     txtAtiv.value = ''
     txtNota.value = ''
@@ -37,6 +41,21 @@ function atualizaTabela() {
     corpoTabela.innerHTML = linhas
 }
 
-function mediaFinal() {
+function atualizaMediaFinal() {
+    const media = calcMediaFinal()
+
+    const mediaF = document.getElementById('mediaF')
+    mediaF.innerHTML = `${media.toFixed(2)}`
     
+    document.getElementById('res').innerHTML = media >= 6 ? 'Aprovado' : 'Reprovado'
+}
+
+function calcMediaFinal() {
+    let somaDasNotas = 0
+    
+    for (let i = 0; i < notas.length; i++) {
+        somaDasNotas += notas[i]
+    }
+    
+    return somaDasNotas / notas.length
 }
